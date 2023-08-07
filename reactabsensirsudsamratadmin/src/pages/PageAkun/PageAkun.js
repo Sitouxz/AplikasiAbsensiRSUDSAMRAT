8817cec2314cdfdedc0bbcfae2183a2423c40721
po3rdlp <leonpolandos53@gmail.com>
2023-08-07 09:54:08 +0800

    Update

--- reactabsensirsudsamratadmin/src/pages/PageAkun/PageAkun.js
+++ reactabsensirsudsamratadmin/src/pages/PageAkun/PageAkun.js
@@ -3,131 +3,118 @@ import {
   HiOutlinePlusSm,
   HiSearch,
   HiOutlinePencil,
-  HiOutlineTrash
-} from 'react-icons/hi';
-import DataTable from 'react-data-table-component';
-
-const columns = [
-  {
-    name: 'Name',
-    selector: (row) => row.name
-  },
-  {
-    name: 'Password',
-    selector: (row) => row.password
-  },
-  {
-    name: 'NIK',
-    selector: (row) => row.nik
-  },
-  {
-    name: 'Bidang/Jabatan',
-    selector: (row) => row.bidangJabatan
-  },
-  {
-    name: 'Action',
-    cell: (row) => (
-      <div>
-        <button
-          type='button'
-          className='btn btn-sm bg-primary-2 text-white hover:bg-primary-3 mr-2'
-          onClick={() => handleEdit(row.id)}>
-          <HiOutlinePencil />
-        </button>
-        <button
-          type='button'
-          className='btn btn-sm bg-red-600 text-white hover:bg-red-700'
-          onClick={() => handleDelete(row.id)}>
-          <HiOutlineTrash />
-        </button>
-      </div>
-    )
-  }
-];
-
-const data = [
-  {
-    id: 1,
-    name: 'John Doe',
-    password: '********', // Replace this with actual password hash if applicable
-    nik: '123456789',
-    bidangJabatan: 'Software Engineer',
-    presence: 'red'
-  },
-  {
-    id: 2,
-    name: 'Jane Smith',
-    password: '********',
-    nik: '987654321',
-    bidangJabatan: 'UI/UX Designer',
-    presence: 'green'
-  }
-  // Add more data objects as needed
-];
-
-const customStyles = {
-  // rows: {
-  //   style: {
-  //     minHeight: '72px' // override the row height
-  //   }
-  // },
-  headCells: {
-    style: {
-      fontWeight: 'bold'
-    }
-  }
-  // cells: {
-  //   style: {
-  //     paddingLeft: '8px', // override the cell padding for data cells
-  //     paddingRight: '8px'
-  //   }
-  // }
-};
-
-// You can define handleEdit and handleDelete functions to handle edit and delete actions
-const handleEdit = (id) => {
-  console.log(`Edit button clicked for row with id: ${id}`);
-};
-
-const handleDelete = (id) => {
-  console.log(`Delete button clicked for row with id: ${id}`);
-};
+  HiOutlineTrash,
+} from "react-icons/hi";
+import DataTable from "react-data-table-component";
+import ModalCreate from "./ModalCreate";
+import ModalEdit from "./ModalEdit";
+import { useState } from "react";
 
 export default function PageAkun() {
-  // const [user, setUser] = useState([]);
+  const [modalOpenCreate, setModalOpenCreate] = useState(false);
+  const [modalOpenEdit, setModalOpenEdit] = useState(false);
+  const [selectedData, setSelectedData] = useState(null);
 
-  // useEffect(() => {
-  //   // get data here using axios
-  // }, []);
+  const openModalCreate = () => {
+    setModalOpenCreate(true);
+  };
+  const closeModalCreate = () => {
+    setModalOpenCreate(false);
+  };
+  const openModalEdit = (dataUser) => {
+    setModalOpenEdit(true);
+    setSelectedData(dataUser);
+  };
+  const closeModalEdit = () => {
+    setModalOpenEdit(false);
+  };
+
+  const tableData = [
+    {
+      id: 1,
+      name: "John Doe",
+      password: "123456",
+      nik: "123456789",
+      bidang: "Marketing",
+    },
+    {
+      id: 2,
+      name: "Jane Smith",
+      password: "abcdef",
+      nik: "987654321",
+      bidang: "Finance",
+    },
+  ];
+
   return (
     <div>
-      <h1 className='text-xl font-medium'>Akun</h1>
-      <div className='flex flex-col gap-3'>
-        <div className='flex justify-end items-center gap-3'>
+      <h1 className="text-xl font-medium">Akun</h1>
+      <div className="flex flex-col gap-3">
+        <div className="flex justify-end items-center gap-3">
           <button
-            type='button'
-            className='bg-primary-2 py-3 px-8 rounded-md font-semibold text-white flex justify-center items-center gap-2'>
+            type="button"
+            onClick={openModalCreate}
+            className="bg-primary-2 py-3 px-8 rounded-md font-semibold text-white flex justify-center items-center gap-2"
+          >
             <HiOutlinePlusSm />
             Buat Akun
           </button>
         </div>
         {/* Search Bar */}
-        <div className='flex items-center relative w-full'>
-          <HiSearch className='absolute left-4' />
+        <div className="flex items-center relative w-full">
+          <HiSearch className="absolute left-4" />
           <input
-            type='text'
-            placeholder='Type here'
-            className='input input-bordered w-full pl-10'
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full pl-10"
           />
         </div>
-        <p className='text-xs text-slate-500'>12 Akun</p>
         <div>
-          <DataTable
-            columns={columns}
-            data={data}
-            customStyles={customStyles}
-          />
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>password</th>
+                <th>nik</th>
+                <th>bidang</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            {tableData.map((data) => (
+              <tbody>
+                <tr key={data.id}>
+                  <td>{data.name}</td>
+                  <td>{data.password}</td>
+                  <td>{data.nik}</td>
+                  <td>{data.bidang}</td>
+                  <td>
+                    <div className="flex gap-3">
+                      <button
+                        className="text-lg"
+                        onClick={() => openModalEdit(data)}
+                      >
+                        <HiOutlinePencil />
+                      </button>
+                      <button className="text-lg">
+                        <HiOutlineTrash />
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            ))}
+          </table>
         </div>
+        {modalOpenCreate && (
+          <ModalCreate isOpen={modalOpenCreate} onClose={closeModalCreate} />
+        )}
+        {modalOpenEdit && (
+          <ModalEdit
+            data={selectedData}
+            isOpen={modalOpenEdit}
+            onClose={closeModalEdit}
+          />
+        )}
       </div>
     </div>
   );
