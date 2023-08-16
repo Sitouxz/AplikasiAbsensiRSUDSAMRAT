6d87d264847b96268aa77c31775b3aed8aff8230
Saylendra Yasin <saylendrayasin@gmail.com>
2023-08-16 11:51:08 +0800

    fix bug

--- reactabsensirsudsamratadmin/src/pages/PageExample/PageExample.jsx
+++ reactabsensirsudsamratadmin/src/pages/PageExample/PageExample.jsx
@@ -4,7 +4,6 @@ import io from 'socket.io-client';
 const socket = io.connect('http://localhost:3001'); // Ganti PORT dengan port server Anda
 
 export default function PagExample() {
-
   const [title, setTitle] = useState('');
   const [message, setMessage] = useState('');
 
@@ -12,33 +11,33 @@ export default function PagExample() {
     //TODO: Save database before send socket
     socket.emit('message', { title, message });
 
-
- return (
-    <div className="w-96 mx-auto my-8 p-4 bg-white rounded shadow">
-      <h2 className="text-lg font-semibold mb-4">Formulir Chat</h2>
-      <div className="flex">
-        <input
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          placeholder="Judul"
-          className="flex-grow p-2 border border-gray-300 rounded-l"
-        />
-        <input
-          type="text"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          placeholder="Pesan"
-          className="flex-grow p-2 border border-gray-300 rounded-l"
-        />
-        <button
-          type="submit"
-          className="bg-blue-500 text-white px-4 py-2 rounded-r hover:bg-blue-600"
-          onClick={sendMsg}
-        >
-          Kirim
-        </button>
+    return (
+      <div className="w-96 mx-auto my-8 p-4 bg-white rounded shadow">
+        <h2 className="text-lg font-semibold mb-4">Formulir Chat</h2>
+        <div className="flex">
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Judul"
+            className="flex-grow p-2 border border-gray-300 rounded-l"
+          />
+          <input
+            type="text"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder="Pesan"
+            className="flex-grow p-2 border border-gray-300 rounded-l"
+          />
+          <button
+            type="submit"
+            className="bg-blue-500 text-white px-4 py-2 rounded-r hover:bg-blue-600"
+            onClick={sendMsg}
+          >
+            Kirim
+          </button>
+        </div>
       </div>
-    </div>
-  )
+    );
+  };
 }
