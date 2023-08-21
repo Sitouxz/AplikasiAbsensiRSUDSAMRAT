57f2d7307c56428d55429acf896dcac9788adf26
andreydesayli <andreydesaylibororing@gmail.com>
2023-08-21 14:41:14 +0800

    added async storage to store access token

--- RNAbsensiRSUDSAMRAT/src/pages/Login/index.tsx
+++ RNAbsensiRSUDSAMRAT/src/pages/Login/index.tsx
@@ -1,20 +1,22 @@
-import { Image, StyleSheet, Text, View, SafeAreaView, TextInput, ScrollView, Dimensions, Alert } from 'react-native'
-import React, { useState } from 'react'
-import { Ilustration3, Logo } from '../../assets/images'
-import Button from '../../components/Button'
-import Gap from '../../components/Gap'
+import { Image, StyleSheet, Text, View, SafeAreaView, TextInput, ScrollView, Dimensions, Alert } from 'react-native';
+import React, { useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Ilustration3, Logo } from '../../assets/images';
+import Button from '../../components/Button';
+import Gap from '../../components/Gap';
 import axios from 'axios';
 
 const Login = ({navigation}: any) => {
-    const [nik, setNik] = useState('')
-    const [password, setPassword] = useState('')
+    const [nik, setNik] = useState('');
+    const [password, setPassword] = useState('');
+    const [dataUser, setDataUser] = useState();
     
     const { width: screenWidth, height: screenHeight } = Dimensions.get('window');
     
     const imageWidth = screenWidth * 1;
     const imageHeight = imageWidth * (2 / 1);
     
-    const url = 'http://192.168.88.208:3001/api/auth/login'; //local
+    const url = 'http://192.168.142.208:3001/api/auth/login'; //local
     const data = {
         "nik": nik,
         "password": password
@@ -23,11 +25,37 @@ const Login = ({navigation}: any) => {
         'Content-Type': 'application/json'
     };
 
+    const storeAccessToken = async (token) => {
+        try {
+            await AsyncStorage.setItem('access_token', token);
+            console.log('Token berhasil disimpan.');
+        } catch (error) {
+            console.log('Gagal menyimpan token:', error);
+        }
+    };
+
     const handleClickLogin = () => {
         axios.post(url, data, {headers})
         .then(function (response) {
-            navigation?.replace('Tabs');
-            console.log('test')
+            const { access_token } = response.data.data
+            setDataUser(access_token)
+            storeAccessToken(access_token);
+            
+            if(access_token){
+                navigation?.replace('Tabs');
+            } else {
+                console.log("No access token found!")
+                Alert.alert(
+                    '',
+                    'Something wrong when access your account!',
+                    [
+                        {
+                            text: 'OK',
+                            style: 'default',
+                        },
+                    ],
+                )
+            }
         })
         .catch(function (error) {
             console.log('error:',error);
