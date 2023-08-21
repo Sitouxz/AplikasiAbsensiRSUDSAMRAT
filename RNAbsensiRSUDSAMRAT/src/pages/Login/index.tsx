550a12a79ea542a9dab22abec28fdaae956e5f52
andreydesayli <andreydesaylibororing@gmail.com>
2023-08-21 13:25:38 +0800

    added endpoint for login

--- RNAbsensiRSUDSAMRAT/src/pages/Login/index.tsx
+++ RNAbsensiRSUDSAMRAT/src/pages/Login/index.tsx
@@ -1,17 +1,48 @@
-import { Image, StyleSheet, Text, View, SafeAreaView, TextInput, ScrollView, Dimensions} from 'react-native'
+import { Image, StyleSheet, Text, View, SafeAreaView, TextInput, ScrollView, Dimensions, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { Ilustration3, Logo } from '../../assets/images'
 import Button from '../../components/Button'
 import Gap from '../../components/Gap'
+import axios from 'axios';
 
 const Login = ({navigation}: any) => {
     const [nik, setNik] = useState('')
     const [password, setPassword] = useState('')
-
+    
     const { width: screenWidth, height: screenHeight } = Dimensions.get('window');
-
+    
     const imageWidth = screenWidth * 1;
     const imageHeight = imageWidth * (2 / 1);
+    
+    const url = 'http://192.168.88.208:3001/api/auth/login'; //local
+    const data = {
+        "nik": nik,
+        "password": password
+    };
+    const headers = {
+        'Content-Type': 'application/json'
+    };
+
+    const handleClickLogin = () => {
+        axios.post(url, data, {headers})
+        .then(function (response) {
+            navigation?.replace('Tabs');
+            console.log('test')
+        })
+        .catch(function (error) {
+            console.log('error:',error);
+            Alert.alert(
+                'NIK/Password tidak sesuai!',
+                'Pastikan NIK dan Password di isi dengan benar.',
+                [
+                    {
+                        text: 'OK',
+                        style: 'default',
+                    },
+                ],
+            )
+        });
+    };
 
     return (
         <SafeAreaView style={styles.page}>
@@ -37,7 +68,7 @@ const Login = ({navigation}: any) => {
                         placeholder='Kata Sandi'
                         />
                     <Gap height={82}/>
-                    <Button title="Log in" onPress={() => navigation.replace('Tabs')}/> 
+                    <Button title="Log in" onPress={handleClickLogin}/> 
                 </View>
             </ScrollView>
         </SafeAreaView>
