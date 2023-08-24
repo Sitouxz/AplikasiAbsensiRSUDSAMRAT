9d658fb69d21e2da498d7c41b849c01b617321a4
Sitouxz <yehezkielowen0901@gmail.com>
2023-08-24 23:20:54 +0800

    added padding

--- reactabsensirsudsamratadmin/src/App.js
+++ reactabsensirsudsamratadmin/src/App.js
@@ -129,7 +129,7 @@ export default function App() {
                 </div>
               </div>
             )}
-            <div>
+            <div className='p-3'>
               <Routes>
                 {tokenExpired && (
                   <>
