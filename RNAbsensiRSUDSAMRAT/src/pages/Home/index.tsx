e9fead781115235b332825fef70ab30154a82b33
andreydesayli <andreydesaylibororing@gmail.com>
2023-08-29 14:33:50 +0800

    added total working days, check in time, & check out time on home page

--- RNAbsensiRSUDSAMRAT/src/pages/Home/index.tsx
+++ RNAbsensiRSUDSAMRAT/src/pages/Home/index.tsx
@@ -8,6 +8,9 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Home = () => {
     const [name, setName] = useState('');
+    const [totalDays, setTotalDays] = useState(0);
+    const [checkInTime, setCheckInTime] = useState('');
+    const [checkOutTime, setCheckOutTime] = useState('');
 
     const getNik = async () => {
         try {
@@ -19,6 +22,12 @@ const Home = () => {
                     const getEmployeeId = response.data.employeeId;
                     const conEmployeeId = getEmployeeId.toString();
                     setEmployeeId(conEmployeeId);
+
+                    const date = String(new Date().getDate()).padStart(2, '0'); 
+                    const month = String(new Date().getMonth() + 1).padStart(2, '0'); 
+                    const year = String(new Date().getFullYear()).padStart(2, '0');
+                    const getDate =  year + '-' + month + '-' + date;
+                    getScheduleTime(getDate, conEmployeeId);
                 }).catch(function(error){
                     console.log('error:', error)
                 })
@@ -29,14 +38,59 @@ const Home = () => {
         }
     }
 
+    const getScheduleTime = async (attendanceDate, employeeId) => {
+        await axios.get(`http://rsudsamrat.site:9999/api/v1/dev/schedule`)
+        .then((response) => {
+            const convertEmployeeId = parseInt(employeeId);
+            const startTime = response.data.filter(schedule => 
+                schedule.scheduleDate === attendanceDate &&
+                schedule.employees.some(employee => employee.employeeId === convertEmployeeId)
+            )
+            .map(schedule => schedule.shift.start_time);
+
+            const endTime = response.data.filter(schedule => 
+                schedule.scheduleDate === attendanceDate &&
+                schedule.employees.some(employee => employee.employeeId === convertEmployeeId)
+            )
+            .map(schedule => schedule.shift.end_time);
+            
+            setCheckInTime(startTime[0]);
+            setCheckOutTime(endTime[0]);
+        }).catch((err) => {
+            console.log('error when access endpoint:', err)
+        });
+    }
     
     const setEmployeeId = async (employeeId) => {
         try {
             await AsyncStorage.setItem('employeeId', employeeId);
             console.log('berhasil menyimpan employee id :', employeeId);
+            getData(employeeId);
         } catch (error) {
             console.log('error:', error);
         }
+    }
+
+    const getData = async (employeeId) => {
+        const url = `http://rsudsamrat.site:9999/api/v1/dev/attendances/filter?employeeId=${employeeId}`;
+        await axios.get(url)
+        .then(function (response) {
+            const uniqueScheduleCounts = {};
+            response.data.forEach(item => {
+                const scheduleDate = item.attendances[0].scheduleDate;
+                const scheduleId = item.scheduleId;
+                if (!uniqueScheduleCounts[scheduleDate]) {
+                    uniqueScheduleCounts[scheduleDate] = new Set();
+                }
+                uniqueScheduleCounts[scheduleDate].add(scheduleId);
+            });
+
+            const numberOfUniqueDates = Object.keys(uniqueScheduleCounts).length;
+            setTotalDays(numberOfUniqueDates);
+        })
+        .catch(function (error) {
+            console.log('error:',error);
+        });
     }
 
     useEffect(() => {
@@ -59,11 +113,11 @@ const Home = () => {
                 <View style={styles.contentContainer}>
                     <Text style={styles.text1}>Today Attendance</Text>
                     <View style={styles.cardContainer}>
-                        <AttendanceCard icon={require('./../../assets/icons/Signin.png')} title="Check In" time="08.00 am" addInfo="On Time"/>
-                        <AttendanceCard icon={require('./../../assets/icons/Signout.png')} title="Check Out" time="16.00 am" addInfo="Go Home"/>
+                        <AttendanceCard icon={require('./../../assets/icons/Signin.png')} title="Check In" time={checkInTime} addInfo="On Time"/>
+                        <AttendanceCard icon={require('./../../assets/icons/Signout.png')} title="Check Out" time={checkOutTime} addInfo="Go Home"/>
                     </View>
                     <View style={styles.additionalCard}>
-                        <AttendanceCard icon={require('./../../assets/icons/MiniCalendar.png')} title="Total Days" time="28" addInfo="Working Days"/>
+                        <AttendanceCard icon={require('./../../assets/icons/MiniCalendar.png')} title="Total Days" time={totalDays} addInfo="Working Days"/>
                     </View>
                 </View>
                 <View style={styles.announcementContainer}>
