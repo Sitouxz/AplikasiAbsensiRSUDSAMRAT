83e4ba48f6a4ce6efe935ed2b3f0a488cdd28bf0
Saylendra Yasin <saylendrayasin@gmail.com>
2023-09-24 14:48:56 +0800

    update: - get property image checkin and checkout - send imgCheckIn and imgCheckOut to modal bukti - implement changes for image - decode img src from base64

--- reactabsensirsudsamratadmin/src/pages/PageAbsensi/ModalBukti.jsx
+++ reactabsensirsudsamratadmin/src/pages/PageAbsensi/ModalBukti.jsx
@@ -3,7 +3,8 @@ import Popup from 'reactjs-popup';
 import api from '../../config/axios';
 import { HiOutlineX } from 'react-icons/hi';
 
-const ModalBukti = forwardRef((image, ref) => {
+const ModalBukti = forwardRef((props, ref) => {
+  const { imageCheckIn, imageCheckOut } = props;
   const [isCheckedIn, setIsCheckedIn] = useState(false);
 
   const toggleCheckIn = () => {
@@ -14,43 +15,56 @@ const ModalBukti = forwardRef((image, ref) => {
     ref.current.close();
   };
 
+  const getImageToDisplay = () => {
+    if (isCheckedIn) {
+      return imageCheckIn;
+    } else {
+      return imageCheckOut;
+    }
+  };
+
+  const imageToDisplay = getImageToDisplay();
+
   return (
     <Popup
       ref={ref}
       modal
       onClose={closeModal}
-      contentStyle={{ borderRadius: '12px', padding: '0', width: '28rem' }}>
+      contentStyle={{ borderRadius: '12px', padding: '0', width: '28rem' }}
+    >
       {(close) => (
-        <div className='relative p-6 overflow-hidden'>
-          <div className='flex flex-col items-center gap-4 mt-4 mb-2'>
+        <div className="relative p-6 overflow-hidden">
+          <div className="flex flex-col items-center gap-4 mt-4 mb-2">
             <button
-              className='absolute block cursor-pointer top-1 right-1'
-              onClick={close}>
-              <HiOutlineX className='text-2xl text-gray-500' />
+              className="absolute block cursor-pointer top-1 right-1"
+              onClick={close}
+            >
+              <HiOutlineX className="text-2xl text-gray-500" />
             </button>
-            <div className='flex gap-2 text-xl font-semibold'>
+            <div className="flex gap-2 text-xl font-semibold">
               <button
                 className={`${isCheckedIn ? 'text-primary-2' : ''}`}
-                onClick={toggleCheckIn}>
+                onClick={toggleCheckIn}
+              >
                 Check in
               </button>
               <button
                 className={` ${!isCheckedIn ? ' text-primary-2' : ''}`}
-                onClick={toggleCheckIn}>
+                onClick={toggleCheckIn}
+              >
                 Check out
               </button>
             </div>
-            <div className='flex flex-col items-center'>
+            <div className="flex flex-col items-center">
               <span>Selasa, 1 Agustus 2023</span>
               <span>01:30</span>
             </div>
-            <span className='text-xl font-bold'>Nariva Doe</span>
-            <div className='p-3 border-4 rounded-xl border-primary-2'>
-              {/* dummy image */}
+            <span className="text-xl font-bold">Nariva Doe</span>
+            <div className="p-3 border-4 rounded-xl border-primary-2">
               <img
-                src='https://picsum.photos/314/489'
-                alt='bukti'
-                className='object-cover w-[314px] h-[489px] rounded-lg'
+                src={`data:image/png;base64,${imageToDisplay}`}
+                alt="bukti"
+                className="object-cover w-[314px] h-[489px] rounded-lg"
               />
             </div>
           </div>
--- reactabsensirsudsamratadmin/src/pages/PageAbsensi/PageAbsensi.jsx
+++ reactabsensirsudsamratadmin/src/pages/PageAbsensi/PageAbsensi.jsx
@@ -13,6 +13,9 @@ export default function PageAbsensi() {
   const [absences, setAbsences] = useState([]);
   const [filteredAbsences, setFilteredAbsences] = useState([]);
   const modalBuktiRef = React.useRef();
+  const [imgCheckIn, setImgCheckIn] = useState(null);
+  const [imgCheckOut, setImgCheckOut] = useState(null);
+
   // const dispatch = useDispatch();
 
   console.log('absences', absences);
@@ -31,19 +34,19 @@ export default function PageAbsensi() {
   const columns = [
     {
       name: 'Nama',
-      selector: (row) => row.name
+      selector: (row) => row.name,
     },
     {
       name: 'Waktu',
-      selector: (row) => row.time
+      selector: (row) => row.time,
     },
     {
       name: 'Sif',
-      selector: (row) => row.shift
+      selector: (row) => row.shift,
     },
     {
       name: 'Kategori',
-      selector: (row) => row.category
+      selector: (row) => row.category,
     },
     {
       name: 'Presensi',
@@ -61,29 +64,33 @@ export default function PageAbsensi() {
               : 'bg-transparent'
           }`}
         />
-      )
+      ),
     },
     {
       name: 'Bukti',
       cell: (row) => (
         <button
-          type='button'
+          type="button"
           onClick={() => {
+            setImgCheckIn(row.selfieCheckIn);
+            setImgCheckOut(row.selfieCheckOut);
             modalBuktiRef.current.open();
+            // setImage(row.selfieCheckIn);
           }}
-          className='text-white btn btn-sm bg-primary-2 hover:bg-primary-3'>
+          className="text-white btn btn-sm bg-primary-2 hover:bg-primary-3"
+        >
           <HiOutlineEye />
         </button>
-      )
-    }
+      ),
+    },
   ];
 
   const customStyles = {
     headCells: {
       style: {
-        fontWeight: 'bold'
-      }
-    }
+        fontWeight: 'bold',
+      },
+    },
   };
 
   useEffect(() => {
@@ -109,12 +116,20 @@ export default function PageAbsensi() {
             ? new Date(attendance.attendances[0].clockOut)
             : null;
 
+          const selfieCheckIn = attendance.attendances[0]?.selfieCheckIn
+            ? attendance.attendances[0].selfieCheckIn
+            : null;
+
+          const selfieCheckOut = attendance.attendances[0]?.selfieCheckOut
+            ? attendance.attendances[0].selfieCheckOut
+            : null;
+
           const formatWaktu = (waktu) => {
             const options = {
               hour: '2-digit',
               minute: '2-digit',
               second: '2-digit',
-              hour12: false
+              hour12: false,
             };
             return waktu.toLocaleTimeString('en-US', options);
           };
@@ -155,7 +170,9 @@ export default function PageAbsensi() {
             ),
             name: employeeNamesString,
             time: attendance.scheduleDate,
-            shift: attendance.shift.name
+            shift: attendance.shift.name,
+            selfieCheckIn: selfieCheckIn,
+            selfieCheckOut: selfieCheckOut,
           };
         });
         console.log('----------------------', ExtractData);
@@ -212,55 +229,57 @@ export default function PageAbsensi() {
     <div>
       <ModalBukti
         ref={modalBuktiRef}
-        // image={image}
+        imageCheckIn={imgCheckIn}
+        imageCheckOut={imgCheckOut}
         onClose={() => modalBuktiRef.current.close()}
       />
 
-      <h1 className='text-xl font-medium'>Absensi</h1>
-      <div className='flex flex-col gap-3'>
-        <div className='flex items-end justify-end gap-3'>
-          <div className='flex items-center justify-center gap-3'>
-            <div className='w-fit'>
+      <h1 className="text-xl font-medium">Absensi</h1>
+      <div className="flex flex-col gap-3">
+        <div className="flex items-end justify-end gap-3">
+          <div className="flex items-center justify-center gap-3">
+            <div className="w-fit">
               Tanggal:
-              <div className='flex items-center justify-center gap-2'>
+              <div className="flex items-center justify-center gap-2">
                 {/* Aug 21, 2021 */}
                 <input
-                  type='date'
-                  className='input input-bordered'
+                  type="date"
+                  className="input input-bordered"
                   onChange={(e) => setStartDate(e.target.value)}
                 />
               </div>
             </div>
             <span>Sampai</span>
-            <div className='w-fit'>
+            <div className="w-fit">
               Tanggal:
-              <div className='flex items-center justify-center gap-2'>
+              <div className="flex items-center justify-center gap-2">
                 {/* Aug 21, 2021 */}
                 <input
-                  type='date'
-                  className='input input-bordered'
+                  type="date"
+                  className="input input-bordered"
                   onChange={(e) => setEndDate(e.target.value)}
                 />
               </div>
             </div>
           </div>
           <button
-            type='button'
-            className='px-10 py-3 font-semibold text-white rounded-md bg-primary-2'>
+            type="button"
+            className="px-10 py-3 font-semibold text-white rounded-md bg-primary-2"
+          >
             Print PDF
           </button>
         </div>
         {/* Search Bar */}
-        <div className='relative flex items-center w-full'>
-          <HiSearch className='absolute left-4' />
+        <div className="relative flex items-center w-full">
+          <HiSearch className="absolute left-4" />
           <input
-            type='text'
-            placeholder='Cari...'
-            className='w-full pl-10 input input-bordered'
+            type="text"
+            placeholder="Cari..."
+            className="w-full pl-10 input input-bordered"
             onChange={handleSearch}
           />
         </div>
-        <p className='text-xs text-slate-500'>{absences.length} Absen</p>
+        <p className="text-xs text-slate-500">{absences.length} Absen</p>
         <div>
           <DataTable
             columns={columns}
